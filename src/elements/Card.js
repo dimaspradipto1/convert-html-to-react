3266579d2170fa7858aa1137221750bb38c1141e
dimas pradipto <dpradipto8@gmail.com>
2026-02-21 12:11:10 +0700

    refactor: card js

--- src/elements/Card.js
+++ src/elements/Card.js
@@ -1,19 +1,21 @@
 import React from "react";
 import propTypes from "prop-types";
 
-export default function Cart(props) {
-  const className = ["tiles-item"]
-  className.push(props.className)
+export default function Card(props) {
+  const className = ["tiles-item"];
+  className.push(props.className);
   return (
     <div className="tiles-item">
-      <div className={`tiles-item-inner ${props.shadow ? "has-shadow" : ""}`}>
+      <div
+        className={`tiles-item-inner ${props.hasShadow ? "has-shadow" : ""}`}
+      >
         {props.children}
       </div>
     </div>
   );
 }
 
-Card.propTypes ={
-  shadow: propTypes.bool,
-  className: propTypes.string
-}
+Card.propTypes = {
+  hasShadow: propTypes.bool,
+  className: propTypes.string,
+};
